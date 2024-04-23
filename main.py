080056b51fbda0d6a9efb20355d6b6e0c948dd57
ScriptSavvy004 <95116040+Monishkumar2004@users.noreply.github.com>
2024-04-23 22:47:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import io
+import pyqrcode
+
+root = Tk()
+
+def generate_qr():
+    link_name = link_name_entry.get()
+    file_name = link_name + ".png"
+    link = link_entry.get()
+    url = pyqrcode.create(link)
+    url.png(file_name, scale=6)
+    image = ImageTk.PhotoImage(Image.open(file_name))
+    image_label = Label(image= image)
+    image_label.image = image
+    canvas.create_window(250,350, window = image_label)
+
+canvas = Canvas(root, width=500, height=600)
+canvas.pack()
+
+header = Label(root, text= "QR Code Generator", fg="orange red", font=("Courier", 30))
+canvas.create_window(250,50, window=header)
+
+link_name = Label(root, text="Link Name", font= ("Courier", 15), fg= "tomato")
+link_label = Label(root, text="Link", font= ("Courier", 15), fg= "tomato")
+canvas.create_window(250,150, window=link_name)
+canvas.create_window(250,250, window=link_label)
+
+link_name_entry = Entry(root)
+link_entry = Entry(root)
+canvas.create_window(250, 180, window= link_name_entry)
+canvas.create_window(250, 280, window= link_entry)
+
+button = Button(root, text= "Generate", font= ("Courier", 20), fg="red", command= generate_qr)
+canvas.create_window(250, 320, window= button)
+
+root.mainloop()
